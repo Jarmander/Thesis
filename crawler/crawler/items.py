d6d968dcecff351e67ae53abda8ec88ec46038a9
Jarmander <k.alhan@live.nl>
2022-05-02 22:13:50 +0200

    Item file changed

--- crawler/crawler/items.py
+++ crawler/crawler/items.py
@@ -1,14 +1,10 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
-
 import scrapy
+
 
 class Demofile(scrapy.Item):
     # define the fields for your item here like:
     match_id = scrapy.Field()
     event_id = scrapy.Field()
     event_name = scrapy.Field()
-    demo_link = scrapy.Field()
+    file_urls = scrapy.Field()
 
